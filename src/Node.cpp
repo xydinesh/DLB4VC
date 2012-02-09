c93b67760a081fb5856991e5dd54dfa6eff91a90
Dinesh Weerapurage <xydinesh@gmail.com>
2012-02-08 21:32:51 -0500

    fisrt working build

---
+++ src/Node.cpp
@@ -0,0 +1,12 @@
+#include "Node.h"
+
+using namespace dlb;
+
+Node::Node()
+{
+	this->size = 0;
+}
+
+Node::~Node()
+{
+}
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "Node.h"
+#include <iostream>
+
+using namespace std;
+using namespace dlb;
+
+int main()
+{
+	cout << "dinesh" << endl;
+	return 0;
+}
